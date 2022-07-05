6ced147b01b7e2d33bf86e6143d690c1a91621f2
fishyeko <106758525+fishyeko@users.noreply.github.com>
2022-07-05 17:21:14 +0300

    Create 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include "main.h"
-/**
- * print_alphabet - Print alphabet in lowercase using only `_putchar`
- * Return: 0
- **/
-void print_alphabet(void)
-{
+/*
+ * print_alphabet -> Print alphabet in lowercase using only `_putchar`
+ *
+ */
+void print_alphabet(void) {
 	char c = 'a';
 
 	while (c <= 'z')
@@ -13,3 +13,4 @@ void print_alphabet(void)
 		c++;
 	}
 	_putchar('\n');
+}
