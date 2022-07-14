aa744a6e610d92d7bffe597913f7c79fb4e89fd8
fishyeko <106758525+fishyeko@users.noreply.github.com>
2022-07-14 23:48:40 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,12 +8,12 @@
 char *string_toupper(char *c)
 {
 	  int i;
- for (i = 0; c[i] != '\0'; i++)
- {
- if (c[i] > 96 && c[i] < 123)
-  {
+for (i = 0; c[i] != '\0'; i++)
+{
+if (c[i] > 96 && c[i] < 123)
+{
 	 c[i] -= 32;
-  }
-  }
+}
+}
 	return (c);
 }
