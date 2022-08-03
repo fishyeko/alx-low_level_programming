2e9703c5c15b751b237b88d7263498da3a8f70c3
fishyeko <106758525+fishyeko@users.noreply.github.com>
2022-08-03 15:39:48 +0300

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,7 @@
-#include <stddef.h>
-#include <stdlib.h>
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
+# ifndef _FUNCTION_POINTERS_H_
+# define  _FUNCTION_POINTERS_H_
 int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
-#endif
+# endif
